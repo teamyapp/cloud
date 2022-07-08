9551bf9ae6c38ab77676b2c5920a44f2871b4fb2
Magicoder <magicoder20@gmail.com>
2022-07-08 00:02:03 -0700

    Add default value for external_user_label

--- app/dao/sqldb/migrations/20220706230709_update_tables.sql
+++ app/dao/sqldb/migrations/20220706230709_update_tables.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 ALTER TABLE identity_allocated_range RENAME TO allocated_range;
-ALTER TABLE identity_user_link ADD external_user_label VARCHAR(50);
+ALTER TABLE identity_user_link ADD external_user_label VARCHAR(50) NOT NULL DEFAULT NULL;
 
 -- +migrate Down
 ALTER TABLE identity_user_link DROP COLUMN external_user_label;
