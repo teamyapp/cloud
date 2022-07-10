08211cee392bd8a77295e4a326074137fd930607
Magic Coder <magicoder20@gmail.com>
2022-07-09 17:50:55 -0700

    Format user link json (#40)

--- app/entity/user_link.go
+++ app/entity/user_link.go
@@ -1,8 +1,8 @@
 package entity
 
 type UserLink struct {
-	AuthProvider      string `json:"auth_provider"`
-	InternalUserID    uint64 `json:"internal_user_id"`
-	ExternalUserID    string `json:"external_user_id"`
-	ExternalUserLabel string `json:"external_user_label"`
+	AuthProvider      string `json:"authProvider"`
+	InternalUserID    uint64 `json:"internalUserId"`
+	ExternalUserID    string `json:"externalUserId"`
+	ExternalUserLabel string `json:"externalUserLabel"`
 }
