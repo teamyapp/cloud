f41f6d93e02ce06f35c9b795449e585cf1106b98
yijiacc <bestmissjia@gmail.com>
2022-08-02 23:34:57 -0700

    Implement Resource APIs for Authorization service

--- app/service/query.go
+++ app/service/query.go
@@ -15,6 +15,15 @@ type ResourceTypeQuery struct {
 	Limit             *uint64
 }
 
+type ResourceQuery struct {
+	ResourceType      *string
+	ResourceID        *uint64
+	CreatorUserID     *uint64
+	StartCreationTime *time.Time
+	EndCreationTime   *time.Time
+	Limit             *uint64
+}
+
 func queryResourceTypes(resourceTypeEntities []entity.ResourceType, resourceTypeQuery ResourceTypeQuery) []entity.ResourceType {
 	return collect.Filter(resourceTypeEntities, func(resourceTypeEntity entity.ResourceType) bool {
 		if resourceTypeQuery.ResourceType != nil && *resourceTypeQuery.ResourceType != resourceTypeEntity.ResourceType {
@@ -30,6 +39,32 @@ func queryResourceTypes(resourceTypeEntities []entity.ResourceType, resourceType
 		}
 
 		if resourceTypeQuery.EndCreationTime != nil && (*resourceTypeQuery.EndCreationTime).Before(resourceTypeEntity.CreatedAt) {
+			return false
+		}
+
+		return true
+	})
+}
+
+func queryResources(resources []entity.Resource, resourceQuery ResourceQuery) []entity.Resource {
+	return collect.Filter(resources, func(resource entity.Resource) bool {
+		if resourceQuery.ResourceType != nil && *resourceQuery.ResourceType != resource.ResourceType {
+			return false
+		}
+
+		if resourceQuery.ResourceID != nil && *resourceQuery.ResourceID != resource.ResourceID {
+			return false
+		}
+
+		if resourceQuery.CreatorUserID != nil && *resourceQuery.CreatorUserID != resource.CreatorUserID {
+			return false
+		}
+
+		if resourceQuery.StartCreationTime != nil && (*resourceQuery.StartCreationTime).After(resource.CreatedAt) {
+			return false
+		}
+
+		if resourceQuery.EndCreationTime != nil && (*resourceQuery.EndCreationTime).Before(resource.CreatedAt) {
 			return false
 		}
 
