dacb59e2f84c6612b3ae3f894ad5b41a80a0ae57
junoth <heathmyh@gmail.com>
2023-03-09 21:03:40 -0800

    Add error constructor with stacktrace integrated

--- libs/errs/errors.go
+++ libs/errs/errors.go
@@ -10,6 +10,8 @@ import (
 
 type ErrorCode string
 
+const maxStackDepth = 100
+
 const (
 	Unknown           ErrorCode = "Unknown"
 	Cancelled         ErrorCode = "Cancelled"
@@ -34,29 +36,48 @@ const (
 )
 
 type Error struct {
-	Code     ErrorCode
-	EmbedErr error
-	Message  string
+	Code       ErrorCode
+	EmbedErr   error
+	Message    string
+	StackTrace *stackTrace
 }
 
 var _ json.Marshaler = (*Error)(nil)
 
+func NewError(code ErrorCode, message string) Error {
+	stackTrace := newStackTrace(maxStackDepth, 1)
+	return Error{
+		Code:       code,
+		Message:    message,
+		StackTrace: &stackTrace,
+	}
+}
+
 func (e Error) MarshalJSON() ([]byte, error) {
 	fields := map[string]string{
-		"Code":     string(e.Code),
-		"Message":  e.Message,
-		"EmbedErr": formatErr(e.EmbedErr),
+		"Code":       string(e.Code),
+		"Message":    e.Message,
+		"EmbedErr":   formatErr(e.EmbedErr),
+		"StackTrace": e.StackTraceString(),
 	}
 
 	return json.Marshal(fields)
 }
 
+func (e Error) StackTraceString() string {
+	if e.StackTrace == nil {
+		return ""
+	}
+
+	return e.StackTrace.String()
+}
+
 func (e Error) String() string {
-	return fmt.Sprintf("[Code=%v Message=%v EmbedErr=%v]", e.Code, e.Message, formatErr(e.EmbedErr))
+	return fmt.Sprintf("[Code=%v Message=%v EmbedErr=%v, StackTrace=%v]", e.Code, e.Message, formatErr(e.EmbedErr), e.StackTraceString())
 }
 
 func (e Error) ToError() error {
-	return fmt.Errorf("[Code=%v Message=%v EmbedErr=%v]", e.Code, e.Message, formatErr(e.EmbedErr))
+	return fmt.Errorf("[Code=%v Message=%v EmbedErr=%v, StackTrace=%v]", e.Code, e.Message, formatErr(e.EmbedErr), e.StackTraceString())
 }
 
 func MergeErrs(errs []*Error) *Error {
---
+++ libs/errs/stack_trace.go
@@ -0,0 +1,70 @@
+package errs
+
+import (
+	"encoding/json"
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+type frame struct {
+	FunctionName string
+	FileName     string
+	LineNumber   int
+}
+
+func (f frame) shortFunctionName() string {
+	index := strings.LastIndex(f.FunctionName, string(filepath.Separator))
+	funcName := f.FunctionName[index+1:]
+	index = strings.Index(funcName, ".")
+	return funcName[index+1:]
+}
+
+func (f frame) String() string {
+	return fmt.Sprintf("%v:%v %v", f.FileName, f.LineNumber, f.shortFunctionName())
+}
+
+type stackTrace struct {
+	frames []frame
+}
+
+var _ json.Marshaler = (*stackTrace)(nil)
+
+func (s stackTrace) MarshalJSON() ([]byte, error) {
+	return json.Marshal(s.String())
+}
+
+func (s stackTrace) String() string {
+	var frameLines []string
+	for _, fm := range s.frames {
+		frameLines = append(frameLines, fm.String())
+	}
+
+	return strings.Join(frameLines, "\n")
+}
+
+func newStackTrace(maxStackSize int, skipCallers int) stackTrace {
+	callers := make([]uintptr, maxStackSize)
+
+	// skip runtime.Callers and newStackTrace
+	n := runtime.Callers(skipCallers+2, callers)
+	callers = callers[0 : n-1]
+	callersFrames := runtime.CallersFrames(callers)
+
+	var frames []frame
+	for {
+		fm, hasNext := callersFrames.Next()
+		if !hasNext {
+			break
+		}
+
+		frames = append(frames, frame{
+			FunctionName: fm.Function,
+			FileName:     fm.File,
+			LineNumber:   fm.Line,
+		})
+	}
+
+	return stackTrace{frames: frames}
+}
