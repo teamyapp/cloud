ef05a6bd2be39d037c32b866dd60e4c78ee8bdcc
Magicoder <magicoder20@gmail.com>
2023-03-10 20:51:39 -0800

    Clean up code

--- testkit/testkit.go
+++ testkit/testkit.go
@@ -1,7 +1,6 @@
 package testkit
 
 import (
-	"fmt"
 	"net"
 	"os"
 	"strings"
@@ -235,28 +234,4 @@ func StartServiceInstance(
 		panic(internalErr)
 	}()
 	<-waitBootstrapCh
-}
-
-const WebServerHost = "web.backend.cloud"
-const WebServerPort = 80
-const GRPCServerHost = "rpc.backend.cloud"
-const GRPCServerPort = 80
-
-func proxyRoutes(webListenerPort int, gRPCListenerPort int) []networktest.ProxyRoute {
-	return []networktest.ProxyRoute{
-		{
-			Endpoint: fmt.Sprintf("%s:%d", WebServerHost, WebServerPort),
-			MatchTarget: func(addr net.Addr) bool {
-				return addr.Network() == "tcp" &&
-					strings.HasSuffix(addr.String(), fmt.Sprintf(":%d", webListenerPort))
-			},
-		},
-		{
-			Endpoint: fmt.Sprintf("%s:%d", GRPCServerHost, GRPCServerPort),
-			MatchTarget: func(addr net.Addr) bool {
-				return addr.Network() == "tcp" &&
-					strings.HasSuffix(addr.String(), fmt.Sprintf(":%d", gRPCListenerPort))
-			},
-		},
-	}
 }
